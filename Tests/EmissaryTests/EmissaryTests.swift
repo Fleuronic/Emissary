ca94d8ab92f511743b059c9ce980e8543eb894a1
Jordan Kay <jordan@fleuronics-mbp.myfiosgateway.com>
2021-12-10 16:52:44 -0500

    Fix build

--- Tests/EmissaryTests/EmissaryTests.swift
+++
@@ -1,4 +0,0 @@
-import XCTest
-@testable import Emissary
-
-final class EmissaryTests: XCTestCase {}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import EmissaryTests
-
-var tests = [XCTestCaseEntry]()
-tests += EmissaryTests.allTests()
-XCTMain(tests)
